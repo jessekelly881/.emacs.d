24c69a826640bcc51d0cdc3f065cb3fe49313668
Jesse Kelly <kellyjesse881@gmail.com>
2021-01-08 11:40:05 -0600

    refactor: organized modes

--- packages/languages.el
+++ packages/languages.el
@@ -1,32 +1,56 @@
 
 ;;; Code:
-(use-package pug-mode :mode ("\\.pug\\'" . pug-mode))
-(use-package web-mode)
-(use-package coffee-mode :mode ("\\.coffee\\'" . coffee-mode))
-(use-package markdown-mode :mode ("\\.md\\'" . markdown-mode))
+
+;; Programming
+
+(use-package haskell-mode)
+(use-package rust-mode)
+(use-package python-django))
+
 (use-package clojure-mode
   :custom
   (clojure-indent-style 'always-indent))
-(use-package haskell-mode)
-(use-package nand2tetris
-  :init (setq nand2tetris-core-base-dir "~/dev/projects/nand2tetris")
-  :config (add-to-list 'auto-mode-alist '("\\.hdl\\'" . nand2tetris-mode))
-  :mode ("\\.hdl\\'" . nand2tetris-mode))
-(use-package python-django :mode ("\\.py\\'" . python-mode))
+
+
+;; Web
+
+(use-package web-mode)
+(use-package elm-mode)
 (use-package dart-mode)
-(use-package dockerfile-mode)
+
+(use-package coffee-mode
+  :mode ("\\.coffee\\'" . coffee-mode))
+
+(use-package pug-mode
+  :mode ("\\.pug\\'" . pug-mode))
+
 (use-package js2-mode
-  :hook
-  (js-mode . js2-minor-mode))
-(use-package ledger-mode
-  :mode ("\\.ledger\\'")
-  :custom (ledger-clear-whole-transactions t))
-(use-package rust-mode)
+  :hook (js-mode . js2-minor-mode))
+
+
+;; Config file modes
+
 (use-package gitignore-mode)
 (use-package gitattributes-mode)
 (use-package gitconfig-mode)
 (use-package yaml-mode)
-(use-package elm-mode)
+(use-package dockerfile-mode)
+
+(use-package markdown-mode
+  :mode ("\\.md\\'" . markdown-mode))
+
+
+;; Misc
+
+(use-package nand2tetris
+  :init (setq nand2tetris-core-base-dir "~/dev/projects/nand2tetris")
+  :config (add-to-list 'auto-mode-alist '("\\.hdl\\'" . nand2tetris-mode))
+  :mode ("\\.hdl\\'" . nand2tetris-mode))
+
+(use-package ledger-mode
+  :mode ("\\.ledger\\'")
+  :custom (ledger-clear-whole-transactions t))
+
 
 (provide 'languages)
 ;;; languages.el ends here
