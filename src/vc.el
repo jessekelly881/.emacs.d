c1ba0b367bc204db5e63cd0eb873650ea8711477
Jesse Kelly <kellyjesse881@gmail.com>
2021-01-11 17:23:32 -0600

    feat(vc): added git lens

--- src/vc.el
+++ src/vc.el
@@ -2,7 +2,13 @@
 ;;; Commentary:
 
 ;;; Code:
-(use-package git-timemachine)
+(use-package git-timemachine
+  :general
+  (nmap :prefix git-key "t" '(git-timemachine :which-key "Timemachine")))
+
+(use-package git-lens
+  :general
+  (nmap :prefix git-key "l" '(git-lens :which-key "Lens")))
 
 (use-package git-gutter
   :custom
@@ -21,15 +27,14 @@
   (git-commit-major-mode 'org-mode)
   :hook
   (after-save-hook . magit-after-save-refresh-status)
-  (git-commit-mode . evil-normal-state))
+  (git-commit-mode . evil-normal-state)
+  :general
+  (nmap :prefix git-key
+    "s" '(magit :which-key "Status")
+    "b" '(magit-blame-echo :which-key "Blame")))
 
 (use-package magit-todos
   :hook (after-init . magit-todos-mode))
-
-(general-def 'normal :prefix git-key
-  "s" '(magit :which-key "Status")
-  "t" '(git-timemachine :which-key "Timemachine"))
-
 
 (provide 'vc)
 ;;; vc.el ends here
