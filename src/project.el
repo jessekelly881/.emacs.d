054d8f9c2436e878fdb77e58cf62d1a7a355d0eb
Jesse Kelly <kellyjesse881@gmail.com>
2021-01-12 23:05:00 -0600

    feat: added npm-mode

--- src/project.el
+++ src/project.el
@@ -7,3 +7,12 @@
   (nmap :prefix project-key "o"
     '(helm-projectile-switch-project :which-key "Open project"))
   (nmap :prefix project-key "/" '(helm-projectile-ag :which-key "Search")))
+
+ ; Package manager modes
+
+(use-package npm-mode
+  :config (npm-global-mode)
+  :general
+  (nmap :prefix project-key
+    :keymaps '(typescript-mode-map js2-mode-map javascript-mode-map)
+    "d" '(npm-mode-npm-list :which-key "Deps")))
